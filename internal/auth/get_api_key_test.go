b5fcd10416a21f29f84712bbb98f9fe79f82f392
kolaj <thekolaj@gmail.com>
2026-05-01 10:50:34 +0300

    Fix failing test

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -34,7 +34,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			key:       "Authorization",
 			value:     "ApiKey xxxxxx",
-			expect:    "xxxxxxBR",
+			expect:    "xxxxxx",
 			expectErr: "not expecting an error",
 		},
 	}
